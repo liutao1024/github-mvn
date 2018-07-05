c913463b54e208a7fc07275e61c47e0c06fd8ce3
liutao1024 <979497772@qq.com>
2018-07-05 15:18:17 +0800

    补全base,comm,core-deposit,server,task模块

---
+++ spring-mvn-admin/spring-mvn-core/spring-mvn-core-deposit/src/main/java/org/spring/mvn/core/deposit/App.java
@@ -0,0 +1,13 @@
+package org.spring.mvn.core.deposit;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
