419093e918cef371112dc437d69722a8483f5297
liutao1024 <979497772@qq.com>
2018-07-05 13:30:04 +0800

    整合代码分模块化第一次上传

---
+++ spring-mvn-admin/spring-mvn-web/src/test/java/cn/spring/mvn/web/test/WebTest.java
@@ -0,0 +1,12 @@
+package cn.spring.mvn.web.test;
+
+import org.junit.Test;
+
+import cn.spring.mvn.core.deposit.Deposit;
+public class WebTest {
+	@Test
+	public void Test001(){
+		Deposit s = new Deposit();
+		System.out.println(s);
+	}
+}
