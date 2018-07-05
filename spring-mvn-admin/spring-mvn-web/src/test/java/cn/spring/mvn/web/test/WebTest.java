f6a88124751aea87dfa93366e0942828610f8074
liutao1024 <979497772@qq.com>
2018-07-05 13:47:20 +0800

    Eclipse中修改提交测试

--- spring-mvn-admin/spring-mvn-web/src/test/java/cn/spring/mvn/web/test/WebTest.java
+++ spring-mvn-admin/spring-mvn-web/src/test/java/cn/spring/mvn/web/test/WebTest.java
@@ -3,6 +3,10 @@ package cn.spring.mvn.web.test;
 import org.junit.Test;
 
 import cn.spring.mvn.core.deposit.Deposit;
+/**
+ * @author LiuTao
+ * Eclipse中修改提交测试
+ */
 public class WebTest {
 	@Test
 	public void Test001(){
