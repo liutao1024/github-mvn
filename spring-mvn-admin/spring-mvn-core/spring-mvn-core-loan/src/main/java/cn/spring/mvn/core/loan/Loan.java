a5021c07b04e1431f08fc188e927478d85335d12
liutao1024 <979497772@qq.com>
2018-07-05 15:08:44 +0800

    修改依赖以及引用

---
+++ spring-mvn-admin/spring-mvn-core/spring-mvn-core-loan/src/main/java/cn/spring/mvn/core/loan/Loan.java
@@ -0,0 +1,14 @@
+package cn.spring.mvn.core.loan;
+
+/**
+ * Hello world!
+ *
+ */
+public class Loan {
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+	public static void print(){
+		System.out.println("Loan.print.Hello World!");
+	}
+}
